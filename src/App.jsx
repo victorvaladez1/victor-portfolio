147a214230d75da0f75baefbaed83e5596f8a512
Victor Valadez <vvaladez315@gmail.com>
2025-06-07 10:11:53 -0500

    Add mobile-responsive navbar with toggleable menu for small screens

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,13 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    document.documentElement.style.scrollBehavior = 'smooth';
+    return () => {
+      document.documentElement.style.scrollBehavior = 'auto';
+    };
+  }, []);
+
   return (
     <>
       {/* Nav Bar */}
@@ -34,7 +41,13 @@ function App() {
             className="md:hidden text-white focus:outline-none"
             onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
           >
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              className={`w-6 h-6 transform transition-transform duration-300 ${mobileMenuOpen ? 'rotate-90' : ''}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
